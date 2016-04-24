41459b19b4682f1ded00c785a97884afa98888bd
sashavol <admin@sashavol.com>
2016-04-23 22:57:23 -0700

    Updated version.

--- Factory.cs
+++ Factory.cs
@@ -37,7 +37,7 @@ namespace LiveSplit.Spelunky
 
         public Version Version
         {
-            get { return Version.Parse("1.3.0");  }
+            get { return Version.Parse("1.3.1");  }
         }
 
         public string Description
