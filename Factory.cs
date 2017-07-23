dd7b16e638e7aad0617f189c17d9f7c3eb9a27f4
sashavol <sashavol@sashavol.com>
2017-07-23 01:58:58 +0100

    Updated to 1.3.5.

--- Factory.cs
+++ Factory.cs
@@ -37,7 +37,7 @@ namespace LiveSplit.Spelunky
 
         public Version Version
         {
-            get { return Version.Parse("1.3.4");  }
+            get { return Version.Parse("1.3.5");  }
         }
 
         public string Description
